d1b46439ac43f2b61243b6a7237d023fc1764265
Lizandra Debussy <lizandradebussy@hotmail.com>
2023-02-19 16:50:30 -0300

    Implementa as funções read e get_unique_job_types

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,40 +1,24 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path, encoding="utf-8") as file:
+        jobs_reader = csv.DictReader(file)
+        list_jobs = list(jobs_reader)
+        return list_jobs
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    jobs = read(path)
+    job_types = []
+    for row in jobs:
+        job_type = row["job_type"]
+        if job_type not in job_types:
+            job_types.append(job_type)
+    return job_types
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
