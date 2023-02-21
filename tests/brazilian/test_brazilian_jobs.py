dd9bc34b884c67969115a4b363394f82f6ec5e2a
Lizandra Debussy <lizandradebussy@hotmail.com>
2023-02-21 18:31:17 -0300

    Implementação da função test_counter e test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    expected = {
+        "title": "Maquinista",
+        "salary": "2000",
+        "type": "trainee",
+    }
+
+    path = "tests/mocks/brazilians_jobs.csv"
+
+    file_reader = read_brazilian_file(path)[0]
+    assert file_reader == expected
--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+
+    count_python = count_ocurrences(path, "Python")
+    assert count_python == 1639
+
+    count_javascript = count_ocurrences(path, "javascript")
+    assert count_javascript == 122
